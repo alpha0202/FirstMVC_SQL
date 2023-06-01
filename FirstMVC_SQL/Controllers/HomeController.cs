686da496cadcdf1fc9d43b6a192e9caf40cae886
edwin martinez <edwmartinezg@outlook.com>
2023-05-31 22:32:39 -0500

    Agregar archivos de proyecto.

---
+++ FirstMVC_SQL/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using FirstMVC_SQL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FirstMVC_SQL.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
